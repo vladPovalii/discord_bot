e85540a24025cffebfa895dff5e06b1ebcc3996a
vlad <vladpovalii@gmail.com>
2020-03-25 02:33:09 +0200

    up

---
+++ src/globals.js
@@ -0,0 +1,19 @@
+// const { Client } = require('discord.js');
+// const YouTube = require('simple-youtube-api');
+// const { GOOGLE_API_KEY } = require('../constants');
+//
+// const client = new Client({ disableEveryone: true });
+// const youtube = new YouTube(GOOGLE_API_KEY);
+//
+// const queue = new Map();
+//
+// const options = {
+//     repeat: false,
+// };
+//
+// module.exports = {
+//     options,
+//     queue,
+//     youtube,
+//     client,
+// };
